731bfbd101bc68956fa6993d3480aa7646f832e6
LF <lifenglf519@126.com>
2022-08-08 15:33:41 +0800

    单例模式

---
+++ python/03_singleton/singleton.py
@@ -0,0 +1,31 @@
+"""
+单例模式
+内容: 保证一个类只有一个实例, 并提供一个访问它的全局访问点
+角色:
+    单例(Singleton)
+优点:
+    对唯一实例的受控访问
+    单例相当于全局变量, 但防止了命名空间被污染
+"""
+
+from abc import abstractmethod, ABCMeta
+
+
+class Singleton():
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            # 调用父类的__new__方法创建一个实例, 父类是Object
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+class Myclass(Singleton):
+    def __init__(self, a) -> None:
+        self.a = a
+
+
+x = Myclass(10)
+y = Myclass(20)
+print(x.a)
+print(id(x))
+print(y.a)
+print(id(y))
